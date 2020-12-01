9e3e6b2f4ec37b8031647901bfb6fe45c4e8bd84
Nataliia Mosiichuk <nataliia.mosiichuk@oa.edu.ua>
2020-12-02 01:54:53 +0200

    add models

---
+++ Models/Event.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace planerApp.Models
+{
+    public class Event
+    {
+        public int e_id { get; set; }
+        public string e_title { get; set; }
+        public DateTime e_date { get; set; }
+
+    }
+}
---
+++ Models/NotateContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace planerApp.Models
+{
+    public class NotateContext : DbContext
+
+    {
+        public DbSet<Notate> Notates {get; set; }
+        public DbSet<Task> Tasks { get; set; }
+        public DbSet<Event> Events { get; set; }
+        public DbSet<Media> Medias { get; set; }
+    }
+}
