297646bdfd4a202b24eadcfbd396c492a8ed704b
Nataliia Mosiichuk <nataliia.mosiichuk@oa.edu.ua>
2020-12-02 11:39:01 +0200

    1

--- Models/Task.cs
+++ Models/Task.cs
@@ -2,12 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace planerApp.Models
 {
     public class Tassk
     {
-        public int T_id { get; set; }
+
+        [Key]  public int T_id { get; set; }
         public string T_title { get; set; }
         public string T_desc { get; set; }
         public DateTime t_data { get; set; }
