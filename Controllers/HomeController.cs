dcdf37b1a170fd82959927b104e361d8a8800f1d
Nataliia Mosiichuk <nataliia.mosiichuk@oa.edu.ua>
2020-12-02 13:34:19 +0200

    працює вивід

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -3,14 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using planerApp.Models;
 
 namespace planerApp.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        NotateContext db = new NotateContext();
+        public ActionResult Index()        
         {
-            return View();
+            {
+                var notates = db.Notates;
+                ViewBag.Notates = notates;
+                return View(); 
+            }
         }
 
         public ActionResult About()
--- Models/Event.cs
+++ Models/Event.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,7 +8,7 @@ namespace planerApp.Models
 {
     public class Event
     {
-        public int e_id { get; set; }
+        [Key] public int e_id { get; set; }
         public string e_title { get; set; }
         public DateTime e_date { get; set; }
 
