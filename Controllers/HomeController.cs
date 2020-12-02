a10142e2b3a8901709b131059b783b7cafe97f39
Nataliia Mosiichuk <nataliia.mosiichuk@oa.edu.ua>
2020-12-02 11:47:16 +0200

    відновлення коміту

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -3,17 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using planerApp.Models;
 
 namespace planerApp.Controllers
 {
     public class HomeController : Controller
     {
-        NotateContext db = new NotateContext();
         public ActionResult Index()
         {
-            var notates = db.Notates;
-            ViewBag.Notates = notates;
             return View();
         }
 
